88dd63e472e85cf80e16dcc192117739f8bc31ed
Jackson Hurst <jhurst@drew.edu>
2015-12-01 22:37:52 -0500

    Changes

--- Stack/src/submission/Palindrome.java
+++ Stack/src/submission/Palindrome.java
@@ -2,7 +2,14 @@ package submission;
 
 public class Palindrome {
 	public static boolean isPalindrome(String s) {
-		// TODO use a stack to determine if s is palindrome
+		int i = 0;
+		int j = s.length() - 1;
+		while(i < j){
+			if(s.charAt(i) != s.charAt(j))
 		return false;
-	}
+		i++;
+		j--;
+		}
+		return true;
+}
 }
