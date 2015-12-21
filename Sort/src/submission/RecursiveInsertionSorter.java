85cc04955a06eda56d8a91c1c2be47fcf29ec974
Jackson Hurst <jhurst@drew.edu>
2015-12-20 20:57:43 -0500

    changes to hw

--- Sort/src/submission/RecursiveInsertionSorter.java
+++ Sort/src/submission/RecursiveInsertionSorter.java
@@ -1,8 +1,20 @@
 package submission;
 
 public class RecursiveInsertionSorter {
-	public static int[] sort(int[] unsorted) {
-		// TODO implement recursive insertion sort
+	public static <T> int[] sort(T unsorted2, T[] unsorted, int first, int last) {
+		if (first < last)
+		{
+		// sort all but the last entry
+		sort(unsorted2, unsorted, first, last - 1);
+		// insert the last entry in sorted order
+		sort(unsorted[last], unsorted, first, last - 1);
+		} //// TODO implement recursive insertion sort
 		return null;
 	}
+
+	public static int[] sort(int[] unsorted) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
